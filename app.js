7b926da51ca6d59067f400a9b45bb7cf5e289c74
HarveyPy-1 <126327787+HarveyPy-1@users.noreply.github.com>
2023-09-05 18:46:48 +0100

    Get project to a working stage

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash")
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -61,7 +62,6 @@ async function loadItems() {
 	allItems = await Item.find();
 
 	if (allItems.length === 0) {
-
 		try {
 			await insertDefaultItems();
 		} catch (err) {
@@ -74,42 +74,113 @@ async function loadItems() {
 
 loadItems();
 
+// Create listSchema to form database relationships
+const listSchema = {
+	name: {
+		type: String,
+		required: true,
+	},
+	items: [itemsSchema],
+};
+
+const List = mongoose.model("List", listSchema);
+
 app.get("/", (req, res) => {
 	const day = date.getDate();
 
-	res.render("list", { listTitle: day, newListItems: allItems });
+	res.render("list", { listTitle: "Home", newListItems: allItems });
 });
 
 app.post("/", (req, res) => {
 	const itemName = req.body.newItem;
+	const listName = req.body.list;
 
 	const newItem = new Item({
 		name: itemName,
 	});
 
-	newItem.save();
-  allItems = []
-	loadItems();
+	// "Today" was supposed to be passed as the H1, but I stuck with the date instead"
+	if (listName === "Home") {
+		newItem.save();
+		allItems = [];
+		loadItems();
+		res.redirect("/");
+	} else {
+		// 'foundItems' can be any name you want
+		List.findOne({ name: listName }).then((foundItems) => {
+			foundItems.items.push(newItem);
+			foundItems.save();
+			allItems = [];
+			loadItems();
+			res.redirect(`/${listName}`);
+		});
+	}
+});
 
-	res.redirect("/");
+// Dynamic URL
+app.get("/:customListName", (req, res) => {
+	const customListName = _.capitalize(req.params.customListName);
+
+	List.findOne({ name: customListName })
+		.then((foundItems) => {
+			if (!foundItems) {
+				// Create a new List
+				const list = new List({
+					name: customListName,
+					items: defaultItems,
+				});
+
+				list.save();
+				res.redirect(`/${customListName}`);
+			} else {
+				// Show the list
+				res.render("list", {
+					listTitle: customListName,
+					newListItems: foundItems.items,
+				});
+			}
+		})
+		.catch((err) => {
+			console.error(err);
+		});
 });
 
 app.post("/delete", (req, res) => {
-  const checkedItemId = req.body.checkbox
+	const checkedItemId = req.body.checkbox;
+	const listName = req.body.listName;
 
-  Item.findByIdAndDelete(checkedItemId)
-    .then(() => {
-      console.log("Item deleted successfully...")
-    })
-    .catch((err) => {
-      console.error(err)
-    })
+	if (listName === "Home") {
+		Item.findByIdAndDelete(checkedItemId)
+			.then(() => {
+				console.log("Item deleted successfully...");
+			})
+			.catch((err) => {
+				console.error(err);
+			});
 
-  allItems = []
-  loadItems();
+		allItems = [];
+		loadItems();
 
-  res.redirect("/")
-})
+		res.redirect("/");
+	} else {
+		// Find a list (collection) with the name 'listName', $pull(delete) from an array called 'items', a file with the '_id' of 'checkedItemId. Then, callback function
+		List.findOneAndUpdate(
+			{ name: listName },
+			{ $pull: { items: { _id: checkedItemId } } }
+		)
+			.then(() => {
+				console.log("Item from list deleted successfully...");
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+
+    allItems = []
+    loadItems();
+
+    res.redirect(`/${listName}`)
+	}
+});
 
 app.get("/about", (req, res) => {
 	res.render("about");
